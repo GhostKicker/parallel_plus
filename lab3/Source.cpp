e2ca97afc8d7330e35c4a4b01fc54d8155ddf8ff
GhostKicker <32245728+GhostKicker@users.noreply.github.com>
2018-04-27 23:51:12 +0300

    123qweqweqwe

--- lab3/Source.cpp
+++ lab3/Source.cpp
@@ -1,12 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <omp.h>
-#include <Windows.h>
+#include <time.h>
 
 using namespace std;
 
-const int limit = 32;
+int n;
 
 template <typename T>
 inline void deb(T& a)
@@ -17,9 +18,8 @@ inline void deb(T& a)
 }
 
 template<typename T>
-void q_sort_omp(vector<T>& arr, int left = 0, int right = -123)
+void q_sort_omp(vector<T>& arr, int left = 0, int right = n - 1)
 {
-    if (right == -123) right = arr.size() - 1;
     if (left >= right) return;
     int l = left;
     int r = right;
@@ -32,19 +32,26 @@ void q_sort_omp(vector<T>& arr, int left = 0, int right = -123)
             swap(arr[l++], arr[r--]);
     }
 
-#pragma omp parallel sections
+    if (right - left > 2000)
     {
+#pragma omp parallel sections
+        {
 #pragma omp section
-        q_sort_omp(arr, left, r);
+            q_sort_omp(arr, left, r);
 #pragma omp section
-        q_sort_omp(arr, l, right);
+            q_sort_omp(arr, l, right);
+        }
+    }
+    else
+    {
+        q_sort(arr, left, r);
+        q_sort(arr, l, right);
     }
 }
 
 template<typename T>
-void q_sort(vector<T>& arr, int left = 0, int right = -123)
+void q_sort(vector<T>& arr, int left = 0, int right = n - 1)
 {
-    if (right == -123) right = arr.size() - 1;
     if (left >= right) return;
     int l = left;
     int r = right;
@@ -61,36 +68,135 @@ void q_sort(vector<T>& arr, int left = 0, int right = -123)
     q_sort(arr, l, right);
 }
 
+
+template <typename T>
+void mergesort_omp(vector<T>& arr, int l = 0, int r = n - 1)
+{
+    if (r - l < 1)
+    {
+        return;
+    }
+
+    int mid = (l + r) / 2;
+
+    if (r - l > 10000)
+    {
+#pragma omp parallel sections
+        {
+#pragma omp section
+            mergesort_omp(arr, l, mid);
+#pragma omp section
+            mergesort_omp(arr, mid + 1, r); 
+        }
+    }
+    else
+    {
+        mergesort(arr, l, mid);
+        mergesort(arr, mid + 1, r);
+    }
+
+    auto b1 = arr.begin() + l;
+    auto e1 = arr.begin() + mid + 1;
+    auto b2 = arr.begin() + mid + 1;
+    auto e2 = arr.begin() + r + 1;
+
+    vector<int> temp(r - l + 1);
+
+    merge(b1, e1, b2, e2, temp.begin());
+
+#pragma omp parallel for
+    for (int i = 0; i < temp.size(); ++i)
+        arr[l + i] = temp[i];
+
+
+}
+
+template <typename T>
+void mergesort(vector<T>& arr, int l = 0, int r = n - 1)
+{
+    if (r - l < 1)
+    {
+        return;
+    }
+
+    int mid = (l + r) / 2;
+
+    mergesort(arr, l, mid);
+    mergesort(arr, mid + 1, r);
+
+    auto b1 = arr.begin() + l;
+    auto e1 = arr.begin() + mid + 1;
+    auto b2 = arr.begin() + mid + 1;
+    auto e2 = arr.begin() + r + 1;
+
+    vector<int> temp(r - l + 1);
+
+    merge(b1, e1, b2, e2, temp.begin());
+
+    for (int i = 0; i < temp.size(); ++i)
+        arr[l + i] = temp[i];
+}
+
+
+
 int main()
 {
+    clock_t cl1, cl2;
+
+
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    int n;
     cin >> n;
-    vector<double> v(n);
+    vector<int> v(n);
+
+    omp_set_num_threads(100);
 
     for (int i = 0; i < n; i++)
         v[i] = rand();
 
+    //int cur = n;
+
+    //for (auto& it : v)
+    //    it = cur--;
+
+
     auto v1 = v;
     auto v2 = v;
+    auto v3 = v;
+    auto v4 = v;
     
     int time;
 
-    time = GetTickCount();
+    cl1 = clock();
     q_sort_omp(v1);
-    time -= GetTickCount();
-    cout << "parallel sorted array of " << n
-        << " elements in " << double(-time) / 1000.0
+    cl2 = clock();
+    cout << "qsort: parallel: " << n
+        << " elements in " << (cl2 - cl1) / double(CLOCKS_PER_SEC)
         << " seconds" << endl;
-#pragma omp taskwait
-    time = GetTickCount();
+
+    cl1 = clock();
     q_sort(v2);
-    time -= GetTickCount();
-    cout << "ordinary sorted array of " << n
-        << " elements in " << double(-time) / 1000.0
+    cl2 = clock();
+    cout << "qsort: ordinary: " << n
+        << " elements in " << (cl2 - cl1) / double(CLOCKS_PER_SEC)
         << " seconds" << endl;
+
+    cl1 = clock();
+    mergesort_omp(v2);
+    cl2 = clock();
+    cout << "merge: parallel: " << n
+        << " elements in " << (cl2 - cl1) / double(CLOCKS_PER_SEC)
+        << " seconds" << endl;
+
+    cl1 = clock();
+    mergesort(v3);
+    cl2 = clock();
+    cout << "merge: ordinary: " << n
+        << " elements in " << (cl2 - cl1) / double(CLOCKS_PER_SEC)
+        << " seconds" << endl;
+
+
 
 
     return 0;
